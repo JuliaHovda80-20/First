fa6e8e66af74d3fa216d4b019de2da68ff0e6c68
JuliaHovda80-20 <juliaroy33@gmail.com>
2023-06-13 15:37:49 +0300

    HW 19 is done!

---
+++ src/HW_19_3.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+# Поточний шлях до бази даних SQLite
+db_path = 'C:\\Users\\Юлія\\Desktop\\база даних для длмашки.sqlite'
+
+# Встановлення з'єднання з базою даних
+connection = sqlite3.connect(db_path)
+
+# Створення курсора
+cursor = connection.cursor()
+
+# Виконання запиту
+query = "SELECT age, COUNT(*) as user_count FROM users GROUP BY age;"
+cursor.execute(query)
+
+# Отримання результатів
+results = cursor.fetchall()
+for row in results:
+    age = row[0]
+    user_count = row[1]
+    print(f"Age: {age}, User Count: {user_count}")
+
+# Закриття курсора і з'єднання з базою даних
+cursor.close()
+connection.close()
